9b1fa01104563beb5e190700d39f6cb4628f9642
Matan Rosenberg <matan129@gmail.com>
2017-09-10 23:21:32 +0300

    Refactor types

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,24 @@
 extern crate pcap;
 extern crate libusb;
 
+use libusb::Context;
+use libusb::Device as UsbDevice;
+
+use pcap::Device as PcapDevice;
+use pcap::{Active, Capture};
+
 const VENDOR: u16 = 0x046d;
 const PRODUCT: u16 = 0x0a1f;
 
 fn main() {
-    let ctx = libusb::Context::new().unwrap();
-    let g930 = find_device(&ctx);
-
-    match g930 {
+    let ctx = Context::new().unwrap();
+    match find_device(&ctx) {
         Some(device) => monitor_device(&device),
         None => println!("Failed to find G930, quitting...")
-    }
+    };
 }
 
-fn find_device(ctx: &libusb::Context) -> Option<libusb::Device> {
+fn find_device(ctx: &Context) -> Option<UsbDevice> {
     for device in ctx.devices().unwrap().iter() {
         let device_desc = device.device_descriptor().unwrap();
 
@@ -28,16 +32,16 @@ fn find_device(ctx: &libusb::Context) -> Option<libusb::Device> {
     None
 }
 
-fn monitor_device(usb_device: &libusb::Device) {
-    match find_usbmon().map(pcap::Device::open) {
+fn monitor_device(usb_device: &UsbDevice) {
+    match find_usbmon().map(PcapDevice::open) {
         Some(Ok(mut cap)) => capture_device(&mut cap, &usb_device),
         Some(Err(e)) => println!("Failed to open G930 for capturing ({:?})", e),
         None => println!("Could not find USB monitor. Try modprobing for it.")
-    }
+    };
 }
 
-fn find_usbmon() -> Option<pcap::Device> {
-    for pcap_dev in pcap::Device::list().unwrap().into_iter() {
+fn find_usbmon() -> Option<PcapDevice> {
+    for pcap_dev in PcapDevice::list().unwrap().into_iter() {
         if pcap_dev.name.contains("usbmon") {
             return Some(pcap_dev);
         }
@@ -46,7 +50,7 @@ fn find_usbmon() -> Option<pcap::Device> {
     None
 }
 
-fn capture_device(cap: &mut pcap::Capture<pcap::Active>, device: &libusb::Device) {
+fn capture_device(cap: &mut Capture<Active>, device: &UsbDevice) {
     let bpf = format!("ether[11:1] = {} and ether[12:1] = {} and ether[9:1] = 1 and ether[10:1] = 0x83",
                       device.address(),
                       device.bus_number());
@@ -57,5 +61,5 @@ fn capture_device(cap: &mut pcap::Capture<pcap::Active>, device: &libusb::Device
 
     while let Ok(packet) = cap.next() {
         println!("received packet! {:?}", packet);
-    }
+    };
 }
